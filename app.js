30d38abf3cfc9ca870fd4303e6071d6398345f1f
Danil Kondratov <com.kondratov@gmail.com>
2023-02-21 11:41:24 +0400

    cors1

--- app.js
+++ app.js
@@ -7,20 +7,15 @@ const app = express();
 
 const MONGO_URI = process.env.MONGO_URI_LOCAL
 const PORT = process.env.PORT || 4000;
-const corsOpts = {
-    origin: '*',
 
-    methods: [
-        'GET',
-        'POST',
-    ],
-
-    allowedHeaders: [
-        'Content-Type',
-    ],
-};
-
-app.use(cors(corsOpts));
+app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 mongoose.set('strictQuery', true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
